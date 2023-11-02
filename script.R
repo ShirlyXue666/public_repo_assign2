165f74e5bdd0f85ecbb624442a3918cee0834cfc
Yuli Xue <97705962+ShirlyXue666@users.noreply.github.com>
2023-11-02 12:47:49 +0000

    Update script.R
    Include new functionality by adding a second argument to the same function.

--- script.R
+++ script.R
@@ -3,10 +3,11 @@ library(dplyr)
 data(iris)
 
 # A first argument to calculate the sepal width to length ratio
-calc_sepal_width_to_length_ratio <- function(data) {
-        data <- data %>% mutate(sepal_width_to_length_ratio = data[, 2]/data[, 1])
+calc_sepal_width_to_length_ratio <- function(data, value) {
+        data <- data %>% mutate(sepal_width_to_length_ratio = (data[, 2]/data[, 1])*value)
         return(data)
 }
 
-calc_sepal_width_to_length_ratio(iris)
+calc_sepal_width_to_length_ratio(iris, 100)
+calc_sepal_width_to_length_ratio(iris, 10)
 
